5bf93eb0ba99e8da00773dd2bf949f40d2e00d63
Kartikye <kartikye.mittal@gmail.com>
2017-09-27 00:46:13 +0000

    fixed setup

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 
 from setuptools import setup
 
-__author__ = "Engine Bai"
+__author__ = "Kartikye Mittal"
 
 setup(
     name="PyMessager",
-    version="1.0.0",
+    version="1.0.1",
     packages=["pymessager", ],
     license='The MIT License (MIT) Copyright © 2017 Engine Bai.',
     description="A Python SDK and Flask API to develop chatbot on Facebook Message Platform",
-    long_description=open("README", "r").read(),
-    author="Engine Bai",
-    author_email="enginebai@gmail.com",
-    url="https://github.com/enginebai/PyMessager",
+    long_description=open("README.md", "r").read(),
+    author="Kartikye Mittal",
+    author_email="kartikye.mittal@gmail.com",
+    url="https://github.com/kartikye/PyMessager",
     install_requires=[
         "requests"
     ],
